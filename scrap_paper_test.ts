e331eb2dbfcdf31a3fc057de4bc220d7bfb972d1
saronnhong <saronnhong@gmail.com>
2026-09-06 11:13:35 -0700

    changes to favicon, title

--- scrap_paper_test.ts
+++
@@ -1,7 +0,0 @@
-aws s3 sync dist/frontend/browser/ \
-  s3://salk-colony-manager-frontend/ \
-  --delete
-
-aws cloudfront create-invalidation \
-  --distribution-id E1ZXHBAO1ZGLCU \
-  --paths "/*"
